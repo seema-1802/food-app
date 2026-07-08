94b1e22788aee451b646c440e1ba35d85244d0df
seema-1802 <seemathakor257@gmail.com>
2026-07-08 20:50:30 +0530

    update  commit

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../componets/Header/Header'
 import ExploreMenu from '../../componets/ExploreMenu/ExploreMenu'
-import Food from '../../componets/Food/food'
+import Food from '../../componets/Food/Food'
 import AppDownload from '../../componets/AppDownload/AppDownload'
 function Home() {
     const [category,setCategory]=useState("All")
